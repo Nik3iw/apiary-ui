d946a99b90f1492cdde904dd4451b9ab6748abda
Nik <nik3iw@gmail.com>
2026-08-10 16:52:09 +0330

    reset repo

--- lib/components/button/types.ts
+++
@@ -1,9 +0,0 @@
-import type { ThemeMainColor, ThemeMainSize, ThemeMainVariant } from "../../types";
-
-export interface ButtonProps {
-  color?: ThemeMainColor;
-  variant?: ThemeMainVariant;
-  size?: ThemeMainSize;
-  icon?: unknown;
-  isLoading?: boolean;
-}
--- lib/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type ThemeMainColor = "primary" | "secondary" | "neutral" | "success" | "info" | "warning" | "error";
-
-export type ThemeMainVariant = "solid" | "outline" | "ghost";
-
-export type ThemeMainSize = "small" | "medium" | "large";
