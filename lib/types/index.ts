17fc602107799df6fab4e9b2b65aefe6a1a9f903
Nik <nik3iw@gmail.com>
2026-08-10 16:49:06 +0330

    Create index.ts

---
+++ lib/types/index.ts
@@ -0,0 +1,5 @@
+export type ThemeMainColor = "primary" | "secondary" | "neutral" | "success" | "info" | "warning" | "error";
+
+export type ThemeMainVariant = "solid" | "outline" | "ghost";
+
+export type ThemeMainSize = "small" | "medium" | "large";
