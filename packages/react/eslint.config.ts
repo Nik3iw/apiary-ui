029e6b384f649fe9eb0d6bfc5af7fcbab7f36617
Nik <nik3iw@gmail.com>
2026-08-11 15:05:15 +0330

    react: complete template

---
+++ packages/react/eslint.config.ts
@@ -0,0 +1,20 @@
+import { createConfig } from "@nik3iw/eslint-config";
+import { globalIgnores } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default createConfig({
+  platform: "web",
+  configs: {
+    unicorn: true,
+    importX: true,
+  },
+  extends: [
+    globalIgnores(["dist"]),
+
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    },
+  ],
+});
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,11 +1,43 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { fileURLToPath, URL } from "node:url";
+import path from "node:path";
+
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+    dts({
+      bundleTypes: true,
+      insertTypesEntry: true,
+      tsconfigPath: "./tsconfig.build.json",
+      include: ["src"],
+    }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    copyPublicDir: false,
+    emptyOutDir: true,
+    sourcemap: true,
+    lib: {
+      // name: "@apiary-ui/vue",
+      entry: path.resolve(import.meta.dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+      // fileName: (format) => `index.${format}.js`,
+    },
+    rolldownOptions: {
+      // external: ['vue'],
+    },
+  },
+});
